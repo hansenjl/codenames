46f14b806f5e2eded20efccc560e6903124e62af
hansenjl <jenn.leigh.hansen@gmail.com>
2020-06-29 17:51:02 -0700

    Broadcast to channel after word guess

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -12,7 +12,10 @@ class WordsController < ApplicationController
     # need to change space to revealed
     #need to determine if turn ends
 
-    render json: result, status: 201
+    # render json: result, status: 201
+    # send websocket message instead
+    #  GameChannel.broadcast_to("game_#{params[:game_id]}", result)
+    GameChannel.broadcast_to(@game, result)
   end
 
   private
